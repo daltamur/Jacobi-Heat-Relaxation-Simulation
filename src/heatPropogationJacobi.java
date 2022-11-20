702e84a35a76f5c81aa08482e5abdf2a3c86a6f1
daltamur <daltamur@oswego.edu>
2022-11-20 11:25:23 -0500

    functionality added to make the display nicer to look at

--- src/heatPropogationJacobi.java
+++ src/heatPropogationJacobi.java
@@ -1,12 +1,17 @@
+import javax.management.*;
 import javax.swing.*;
 import java.awt.*;
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.*;
+import com.sun.management.OperatingSystemMXBean;
 
 public class heatPropogationJacobi extends JPanel implements constants{
     public static region [][] paintedA;
     public static region [][] paintedB;
     public static boolean startPainting;
     public static boolean paintB;
+
+    public static OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -22,17 +27,25 @@ public class heatPropogationJacobi extends JPanel implements constants{
             // height will store the height of the screen
             int heightFrame = frame.getSize().height;
             double heightMesh = Math.ceil((double) heightFrame/(double) paintedA.length);
+            region[][] currentPaint;
+            if(paintB){
+                currentPaint = paintedB;
+            }else{
+                currentPaint = paintedA;
+            }
                     for (int i = 0; i<paintedA.length; i++){
                         for(int j = 0; j<paintedA[0].length; j++){
-                                double curTemp = paintedB[i][j].getCurrentTemp();
+                                double curTemp = currentPaint[i][j].getCurrentTemp();
                                 if (curTemp > 255){
-                                    curTemp = 255.0;
+                                    double hue = 0;
+                                    float curBrightness = (float) (curTemp*brightnessSlope+brightnessYInt);
+                                    g.setColor(Color.getHSBColor((float) hue, 1, curBrightness));
+                                }else {
+                                    //use the function y = -x + 255 to get the inverses where x=curTemp
+                                    curTemp = -curTemp + 255;
+                                    double hue = curTemp / 360.0;
+                                    g.setColor(Color.getHSBColor((float) hue, 1, 1));
                                 }
-                                //use the function y = -x + 255 to get the inverses where x=curTemp
-                                curTemp = -curTemp + 255;
-                                double hue = curTemp/360.0;
-
-                                g.setColor(Color.getHSBColor((float) hue, 1, 1));
 
                             g.fillRect(curXPos, curYPos, (int) widthMesh, (int) heightMesh);
                             if(j == paintedA[0].length - 1){
@@ -64,29 +77,93 @@ public class heatPropogationJacobi extends JPanel implements constants{
 
     private static void createAndShowGui() {
         heatPropogationJacobi mainPanel = new heatPropogationJacobi();
-        mainPanel.setLayout(new BorderLayout());
-        JLabel title = new JLabel("Title");
-        title.setBackground(Color.black);
-        title.setFont(new Font(title.getFont().toString(), Font.BOLD, 48));
-        title.setForeground(Color.white);
-        title.setText("Heat Propagation Visualization of a Heated Metal Alloy");
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        JLabel name = new JLabel("Name");
-        name.setBackground(Color.black);
-        name.setFont(new Font(title.getFont().toString(), Font.BOLD, 15));
-        name.setForeground(Color.white);
-        name.setText("Authored by Dominic Altamura");
-        mainPanel.add(title, BorderLayout.PAGE_START);
-        mainPanel.add(name,BorderLayout.SOUTH);
         GraphicsEnvironment graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice device = graphics.getDefaultScreenDevice();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(mainPanel);
+        frame.setVisible(true);
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         //show a new floor every fifth of a second
         Timer t = new Timer(10,
                 e -> mainPanel.repaint());
         t.start();
-        frame.setVisible(true);
+        JLabel title = new JLabel("");
+        title.setFont(new Font(title.getFont().toString(), Font.BOLD, 35));
+        title.setForeground(Color.white);
+        title.setBackground(new Color(0, 0, 0, 50));
+        title.setOpaque(true);
+        title.setText("Heat Transfer Visualization on a Sheet of Metal Using a Jacobi Relaxation Technique");
+        String temperatureLeft = "Current temperature: " + S;
+        String temperatureRight = "Current temperature: " + T;
+        c.weighty = .05;
+        c.weightx = .05;
+        c.anchor = GridBagConstraints.NORTH;
+        c.gridx = 0;
+        c.gridy = 0;
+        mainPanel.add(title, c);
+        JLabel subtitle = new JLabel("");
+        subtitle.setFont(new Font(title.getFont().toString(), Font.BOLD, 25));
+        subtitle.setForeground(Color.white);
+        subtitle.setBackground(new Color(0, 0, 0, 50));
+        subtitle.setOpaque(true);
+        subtitle.setText("Calculations Are Running Concurrently on All Cores");
+        c.gridy = 1;
+        c.weighty = 1;
+        c.weightx = 1;
+        mainPanel.add(subtitle, c);
+
+        JLabel name = new JLabel();
+        name.setBackground(Color.black);
+        name.setFont(new Font(title.getFont().toString(), Font.BOLD, 15));
+        name.setForeground(Color.white);
+        name.setBackground(new Color(0, 0, 0, 50));
+        name.setOpaque(true);
+        c.anchor = GridBagConstraints.LAST_LINE_START;
+        c.weighty = .05;
+        c.weightx = .05;
+        c.gridx = 0;
+        c.gridy = 2;
+        mainPanel.add(name, c);
+
+        name.setText("Authored by Dominic Altamura, Class of '23");
+        JLabel Percentage = new JLabel("");
+        Percentage.setFont(new Font(title.getFont().toString(), Font.BOLD, 25));
+        Percentage.setForeground(Color.white);
+        Percentage.setBackground(new Color(0, 0, 0, 50));
+        Percentage.setOpaque(true);
+        Timer changePercentage = new Timer(10, e-> {
+            MBeanServer mbs    = ManagementFactory.getPlatformMBeanServer();
+            ObjectName names    = null;
+            try {
+                names = ObjectName.getInstance("java.lang:type=OperatingSystem");
+            } catch (MalformedObjectNameException malformedObjectNameException) {
+                malformedObjectNameException.printStackTrace();
+            }
+
+            double value = osBean.getSystemCpuLoad()*100.0;
+            Percentage.setText(String.format("Current CPU Usage: %.02f%%", value));
+        });
+        changePercentage.start();
+        c.anchor = GridBagConstraints.LAST_LINE_END;
+        mainPanel.add(Percentage, c);
+
+
+
+        Timer titles = new Timer(180000, e-> {
+            try {
+                displayTitles(title, name, subtitle);
+            } catch (ReflectionException reflectionException) {
+                reflectionException.printStackTrace();
+            } catch (InstanceNotFoundException instanceNotFoundException) {
+                instanceNotFoundException.printStackTrace();
+            } catch (MalformedObjectNameException malformedObjectNameException) {
+                malformedObjectNameException.printStackTrace();
+            }
+        });
+        titles.start();
+
+
         beginProgram = true;
         GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
         Rectangle bounds = environment.getMaximumWindowBounds();
@@ -94,13 +171,27 @@ public class heatPropogationJacobi extends JPanel implements constants{
         GraphicsConfiguration config = device.getDefaultConfiguration();
         System.out.println("Screen Size = " + config.getBounds());
         System.out.println("Frame Size = " + frame.getSize());
+    }
 
+    private static void displayTitles(JLabel title, JLabel name, JLabel subtitle) throws ReflectionException, InstanceNotFoundException, MalformedObjectNameException {
+        title.setVisible(true);
+        name.setVisible(true);
+        subtitle.setVisible(true);
+        Timer erase = new Timer(30000, e->{title.setVisible(false);
+            name.setVisible(false);
+            title.setVisible(false);
+            subtitle.setVisible(false);
+        });
+        erase.setRepeats(false);
+        erase.start();
     }
 
     public static JFrame frame;
     public static boolean beginProgram = false;
     public static double S = 0.0;
     public static double T = 0.0;
+    public static double brightnessSlope;
+    public static double brightnessYInt;
 
 
 
@@ -110,45 +201,62 @@ public class heatPropogationJacobi extends JPanel implements constants{
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
         frame.setVisible(true);
-        int height = 0;
-        int width = 0;
-
-        try {
-             height = Integer.parseInt(JOptionPane.showInputDialog(null,"Height of mesh (preferrable if you make the height a divisor of " + frame.getSize().height));
-        }catch (Exception e){
-            System.out.println(e);
-            System.exit(-1);
-        }
-
-        try {
-           width = Integer.parseInt(JOptionPane.showInputDialog(null,"Width of mesh (preferrable if you make the height a divisor of " + frame.getSize().width));
-        }catch (Exception e){
-            System.out.println(e);
-            System.exit(-1);
-        }
-        frame.setVisible(false);
+        //frame.setVisible(false);
 
         SwingUtilities.invokeLater(heatPropogationJacobi::createAndShowGui);
 
         while (!beginProgram){
             System.out.println("Waiting on gui initialization");
         }
+        int height = 200;//frame.getHeight();
+        int width =300; //frame.getWidth();
+        int prevMod = frame.getHeight()%height;
+        int curMod;
+        while (frame.getHeight()%height != 0){
+                curMod = frame.getHeight()%(height+1);
+                if(curMod <= prevMod) {
+                    prevMod = curMod;
+                    height =  (height + 1);
+                }else {
+                    break;
+                }
+                System.out.println("d "+frame.getHeight()%height);
+        }
+
+        prevMod = frame.getWidth()%width;
+        while (frame.getWidth()%width != 0){
+            curMod = frame.getWidth()%(width+1);
+            if(curMod <= prevMod) {
+                prevMod = curMod;
+                width =  (width + 1);
+                System.out.println(width);
+            }else {
+                break;
+            }
+            System.out.println("f "+frame.getWidth()%width);
+        }
+        System.out.println(height);
+        System.out.println(width);
         runProgram(height, width);
     }
 
     public static void runProgram(int height, int width) throws InterruptedException {
         region[][] a = new region[height][width];
         region[][] b = new region[height][width];
-        S = 1000000;//ThreadLocalRandom.current().nextDouble(100000, 5000000);
-        T= 1000000;//ThreadLocalRandom.current().nextDouble(100000, 5000000);
+        S = 5000;//ThreadLocalRandom.current().nextDouble(255, 2500);
+        T= 5000;//ThreadLocalRandom.current().nextDouble(255, 2500);
+        //formula calculations if the regions heat is greater than 255
+        double greaterTemp = Math.max(S, T);
+        brightnessSlope  = (.5 - 1)/(greaterTemp - 255);
+        brightnessYInt = 1 + -brightnessSlope*255;
 
         for(int i = 0; i<a.length; i++){
             for(int j = 0; j<a[0].length; j++){
-                double metal1Portion = ThreadLocalRandom.current().nextDouble(0,1);
-                double metal2Portion = ThreadLocalRandom.current().nextDouble(0, 1-metal1Portion);
-                double metal3Portion = 1 - metal1Portion - metal2Portion;
+                double metal1Portion = 1.0/3.0;//ThreadLocalRandom.current().nextDouble(0,1);
+                double metal2Portion = 1.0/3.0;//ThreadLocalRandom.current().nextDouble(0, 1-metal1Portion);
+                double metal3Portion = 1.0/3.0;//1 - metal1Portion - metal2Portion;
                 //for metal1Percentage, this one should never be a majority value, as it will always bring the temperature down if it is a majority.
-                double currentTemp = 45;
+                double currentTemp = 0;
                 a[i][j] = new region(false, metal1Portion, metal2Portion, metal3Portion, currentTemp);
                 b[i][j] = new region(false, metal1Portion, metal2Portion, metal3Portion, currentTemp);
             }
@@ -190,24 +298,26 @@ public class heatPropogationJacobi extends JPanel implements constants{
         for (int i = 0; i < a.length; i++) {
 
             for (int j = 0; j < a[0].length; j++) {
-                System.out.print("|");
-                System.out.print(String.format("%.02f", b[i][j].getCurrentTemp()));
-                System.out.print("|");
+                //System.out.print("|");
+                //System.out.print(String.format("%.02f", b[i][j].getCurrentTemp()));
+                //System.out.print("|");
             }
             System.out.println();
         }
 
 
 
-        /*
+
         while(true) {
-            S = ThreadLocalRandom.current().nextDouble(100000, 5000000);
-            T= ThreadLocalRandom.current().nextDouble(100000, 5000000);
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[0].length; j++) {
-                    double currentTemp = 0.0;
-                    a[i][j].changeTemp(currentTemp);
-                    b[i][j].changeTemp(currentTemp);
+            S = ThreadLocalRandom.current().nextDouble(255, 2500);
+            T= ThreadLocalRandom.current().nextDouble(255, 2500);
+            greaterTemp = Math.max(S, T);
+            brightnessSlope  = (.5 - 1)/(greaterTemp - 255);
+            brightnessYInt = 1 + -brightnessSlope*255;
+            for(int i = 0; i<a.length; i++){
+                for(int j = 0; j<a[0].length; j++){
+                    a[i][j].changeTemp(0);
+                    b[i][j].changeTemp(0);
                 }
             }
             a[0][0].changeTemp(S);
@@ -219,7 +329,7 @@ public class heatPropogationJacobi extends JPanel implements constants{
             engine.reinitialize();
         }
 
-         */
+
 
     }
 
@@ -244,9 +354,6 @@ public class heatPropogationJacobi extends JPanel implements constants{
         fourQuad(){}
 
         //when we complete all the forks for each quadrant, find the maximum difference
-
-
-
         public void compute() {
             //fork each quadrant
             q4.fork();
@@ -414,7 +521,7 @@ public class heatPropogationJacobi extends JPanel implements constants{
                                     default:
                                         throw new IllegalStateException("Unexpected value: " + currentConstantIteration);
                                 }
-                                //neighborSummation += a[i-1][j-1].getCurrentTemp() * a[i-1][j-1].getMetal1Percentage() * a[i-1][j-1].getMetal2Percentage() * a[i-1][j-1].getMetal3Percentage();
+
                             }
                             if(topRight){
                                 switch (currentConstantIteration){
@@ -427,7 +534,7 @@ public class heatPropogationJacobi extends JPanel implements constants{
                                     default:
                                         throw new IllegalStateException("Unexpected value: " + currentConstantIteration);
                                 }
-                                //neighborSummation += a[i-1][j+1].getCurrentTemp() * a[i-1][j+1].getMetal1Percentage() * a[i-1][j+1].getMetal2Percentage() * a[i-1][j+1].getMetal3Percentage();
+
                             }
                             if(bottomLeft){
                                 switch (currentConstantIteration){
@@ -440,7 +547,7 @@ public class heatPropogationJacobi extends JPanel implements constants{
                                     default:
                                         throw new IllegalStateException("Unexpected value: " + currentConstantIteration);
                                 }
-                                //neighborSummation += a[i+1][j-1].getCurrentTemp() * a[i+1][j-1].getMetal1Percentage() * a[i+1][j-1].getMetal2Percentage() * a[i+1][j-1].getMetal3Percentage();
+
                             }
                             if(bottomRight){
                                 switch (currentConstantIteration){
@@ -515,45 +622,9 @@ public class heatPropogationJacobi extends JPanel implements constants{
 
                 root.invoke();
 
-                /*
-                System.out.println("A array");
-                for(int i = 0; i<A.length; i++){
-                    for (int j = 0; j<A[0].length; j++){
-                        System.out.print("|");
-                        System.out.print(String.format("%.02f", A[i][j].getCurrentTemp()));
-                        System.out.print("|");
-                    }
-                    System.out.println();
-                }
-
-                System.out.println("B array");
-                for(int i = 0; i<A.length; i++){
-                    for (int j = 0; j<A[0].length; j++){
-                        System.out.print("|");
-                        System.out.print(String.format("%.02f", B[i][j].getCurrentTemp()));
-                        System.out.print("|");
-                    }
-                    System.out.println();
-                }
-
-                 */
-
-
-
-
-
-                if (root.maxDifference <= EPSILON) {
+                if (root.maxDifference <= EPSILON || this.steps == 50000) {
                     System.out.println("Converged after "+ this.steps + " steps");
                     System.out.println("Max Difference: "+ root.maxDifference);
-                    System.out.println("B array");
-                    for(int i = 0; i<A.length; i++){
-                        for (int j = 0; j<A[0].length; j++){
-                            System.out.print("|");
-                            System.out.print(String.format("%.02f", B[i][j].getCurrentTemp()));
-                            System.out.print("|");
-                        }
-                        System.out.println();
-                    }
                     root.maxDifference = 0.0;
                     this.steps = 0;
                     root.reinitialize();
